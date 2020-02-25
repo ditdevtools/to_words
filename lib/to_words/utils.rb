a26e6364c28f4133c750d2e41502c57dac66445e
KRG - Department of Information Technology <59687011+ditdevtools@users.noreply.github.com>
2020-02-25 15:13:55 +0300

    Update utils.rb

--- lib/to_words/utils.rb
+++ lib/to_words/utils.rb
@@ -12,7 +12,6 @@ module ToWords
     end
 
     def higher_than_hundred(hundred, remaining, counter)
-      byebug
       century = UNDER_HUNDRED[hundred]
       if remaining != 0
         return century + " سەد " + UNDER_HUNDRED[remaining] if counter != 0
